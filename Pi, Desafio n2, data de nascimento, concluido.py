b03f0563e009c0ba0ea6bcb7142961f049bbd27b
arthurbenedito <arthurbendito1911@gmail.com>
2026-02-09 14:32:09 -0300

    Create Pi, Desafio n2, data de nascimento, concluido.py

---
+++ Pi, Desafio n2, data de nascimento, concluido.py
@@ -0,0 +1,4 @@
+ano=input ('Em qual ano você nasceu ?')
+mês=input ('Em qual mês você nasceu ?')
+dia=input ('Em qual dia você nasceu ?')
+print( 'Você nasceu no dia ' +dia+ ' de '+mês+ ' de ' +ano+ '. Correto ? ' )
